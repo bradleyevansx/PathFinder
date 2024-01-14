56d722962e488f2521ea9bcd5330584272611d0c
bradleyevansx <bradleyevans.x@gmail.com>
2024-01-14 18:41:15 -0500

    header size fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import ColorModeSwitch from "./ColorModeSwitch";
 
 const Header = () => {
   return (
-    <header className="hidden h-16 md:flex justify-between items-center px-5 border-b border-gray-200  dark:border-neutral-800">
+    <header className="hidden w-full h-16 md:flex justify-between items-center px-5 border-b border-gray-200  dark:border-neutral-800">
       <h1 className="text-lg font-bold m-0 w-fit">
         Path Finding Algorithm Visualizer
       </h1>
